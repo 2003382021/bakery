d00cc59e2bdad97358ba3dea5f734af53f403d0b
Xcharmine <95407796+2003382021@users.noreply.github.com>
2024-02-09 14:32:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
-const navbar = document.querySelector('.navbar');
-const menuBar = document.querySelector('.menu');
-const closeIcon = document.querySelector('.close');
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.reg-link');
-const btnpopup = document.querySelector('.loginbtn');
-const iconClose = document.querySelector('.icon-close');
+let wrapper = document.querySelector('.wrapper');
+let loginLink = document.querySelector('.login-link');
+let registerLink = document.querySelector('.reg-link');
+let btnpopup = document.querySelector('.loginbtn');
+let iconClose = document.querySelector('.icon-close');
 
 
- menuBar.addEventListener('click', ()=>{
-    navbar.classList.add('active');
-});
-
-closeIcon.addEventListener('click', ()=>{
-    navbar.classList.remove('active');
-});
-
-
-registerLink.addEventListener('click', ()=>{
-    wrapper.classList.add('active');
-});
 
 loginLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
 });
 
+btnpopup.addEventListener('click', ()=>{
+    wrapper.classList.toggle('active-popup');
+});
+
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+});
+
+iconClose.addEventListener('click', ()=>{
+    wrapper.classList.remove('active-popup');
+});
+
+
+
+const app = () =>{
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        listProduct = data;
+        console.log(listProduct);
+    })
+}
+app()
+
+all.addEventListener('click', (event) => {
+    let click = event.target;
+    if(click.classList.contains('header')){
+        let product_id = position.dataset.id;
+        alert(product_id);
+    }
+}
+)
+
+
+
+
+const navigation = document.querySelector('.navbar');
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', ()=>{
+    wrapper.classList.remove('active-popup');
+});
+
+menu.addEventListener('click', ()=>{
+    navigation.classList.toggle('active');
+});
 
 
 
@@ -41,3 +72,5 @@ function back(){
     document.getElementById('payment').style.display='none';
     document.getElementById('order').style.display='initial';
 }
+
+
